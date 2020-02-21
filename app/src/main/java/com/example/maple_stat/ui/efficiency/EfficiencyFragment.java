3f752e39b122dbfb9ec72cec0522e554c1341ef0
myeongjinkim <kiakd2@naver.com>
2020-02-22 01:10:27 +0900

    Add stat calculator to fragment_efficiency

--- app/src/main/java/com/example/maple_stat/ui/efficiency/EfficiencyFragment.java
+++ app/src/main/java/com/example/maple_stat/ui/efficiency/EfficiencyFragment.java
@@ -26,23 +26,14 @@ public class EfficiencyFragment extends Fragment {
                 ViewModelProviders.of(this).get(EfficiencyViewModel.class);
         View root = inflater.inflate(R.layout.fragment_efficiency, container, false);
 
-        Spinner spinner_stat1 = (Spinner) root.findViewById(R.id.my_stat1);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
-                R.array.my_stat, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner_stat1.setAdapter(adapter1);
 
-        Spinner spinner_stat2 = (Spinner) root.findViewById(R.id.my_stat2);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.my_stat, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner_stat2.setAdapter(adapter2);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner_stat = (Spinner) root.findViewById(R.id.my_stat);
+        spinner_stat.setAdapter(adapter);
+
         return root;
     }
 }
