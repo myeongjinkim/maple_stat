20321c4e439ebd97f64b2cb46cc2c4e5ac7a619a
myeongjinkim <kiakd2@naver.com>
2020-02-27 02:28:50 +0900

    Change fragment_stat_compare UI to round and change color

--- app/src/main/java/com/example/maple_stat/ui/efficiency/EfficiencyCompareFragment.java
+++ app/src/main/java/com/example/maple_stat/ui/efficiency/EfficiencyCompareFragment.java
@@ -17,30 +17,32 @@ import com.example.maple_stat.R;
 
 public class EfficiencyCompareFragment extends Fragment {
 
+    private ArrayAdapter<CharSequence> adapter1;
+    private ArrayAdapter<CharSequence> adapter2;
 
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        adapter1 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.stat_list, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adapter2 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.stat_standard, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_efficiency_compare, container, false);
 
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
-                R.array.stat_list, android.R.layout.simple_spinner_item);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner_stat_compare1 = (Spinner) root.findViewById(R.id.stat_compare1);
         spinner_stat_compare1.setAdapter(adapter1);
 
         Spinner spinner_stat_compare2 = (Spinner) root.findViewById(R.id.stat_compare2);
         spinner_stat_compare2.setAdapter(adapter1);
 
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
-                R.array.stat_standard, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner_stat_standard = (Spinner) root.findViewById(R.id.stat_standard);
         spinner_stat_standard.setAdapter(adapter2);
-
 
         return root;
     }
