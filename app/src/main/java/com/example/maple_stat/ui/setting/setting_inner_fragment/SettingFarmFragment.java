c1db410352e1a8126b8f8dd1a7dcf260487b292d
myeongjinkim <kiakd2@naver.com>
2020-07-09 15:17:23 +0900

    Change UI of fragment_setting_pet

--- app/src/main/java/com/example/maple_stat/ui/setting/setting_inner_fragment/SettingFarmFragment.java
+++ app/src/main/java/com/example/maple_stat/ui/setting/setting_inner_fragment/SettingFarmFragment.java
@@ -1,6 +1,7 @@
 package com.example.maple_stat.ui.setting.setting_inner_fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import com.example.maple_stat.R;
 import com.example.maple_stat.databinding.FragmentSettingFarmBinding;
+
+import static android.content.ContentValues.TAG;
 
 public class SettingFarmFragment extends Fragment {
 
@@ -33,5 +36,12 @@ public class SettingFarmFragment extends Fragment {
         binding.setFragment(this);
 
         return root;
+    }
+    public void pressFarmDeleteButton(View view) {
+        Log.i(TAG,"지우기");
+    }
+
+    public void pressFarmAddButton(View view) {
+        Log.i(TAG,"더하기");
     }
 }
