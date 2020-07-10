1e816c8974d38f532dc05cbf43396324b31ede7a
myeongjinkim <kiakd2@naver.com>
2020-07-10 17:49:50 +0900

    Implement UI of fragment_setting_etc

--- app/src/main/java/com/example/maple_stat/ui/setting/setting_inner_fragment/SettingEtcFragment.java
+++ app/src/main/java/com/example/maple_stat/ui/setting/setting_inner_fragment/SettingEtcFragment.java
@@ -1,6 +1,7 @@
 package com.example.maple_stat.ui.setting.setting_inner_fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import com.example.maple_stat.R;
 import com.example.maple_stat.databinding.FragmentSettingEtcBinding;
+
+import static android.content.ContentValues.TAG;
 
 public class SettingEtcFragment extends Fragment {
 
@@ -33,5 +36,16 @@ public class SettingEtcFragment extends Fragment {
         binding.setFragment(this);
 
         return root;
+    }
+    public void pressEtcDeleteButton(View view) {
+        Log.i(TAG,"지우기");
+    }
+
+    public void pressEtcAddButton(View view) {
+        Log.i(TAG,"더하기");
+    }
+
+    public void pressEtcBuffOn0Button(View view) {
+        Log.i(TAG,"0");
     }
 }
